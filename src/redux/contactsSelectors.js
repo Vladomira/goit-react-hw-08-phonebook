e20e007867cdb3c699043fb2a63d7b152ae1cc31
Vladomira <juliavladomira@gmail.com>
2021-11-17 14:52:56 +0300

    corrects selector

--- src/redux/contactsSelectors.js
+++ src/redux/contactsSelectors.js
@@ -4,16 +4,17 @@ export const getContacts = (state) => state.contacts.items;
 export const getFiltredValue = (state) => state.contacts.filter;
 export const getLoading = (state) => state.contacts.isLoading;
 
-export const getVisibleContacts = (state) => {
-  const allContacts = getContacts(state);
-  const contactsFilter = getFiltredValue(state);
-  const normalizeFilter = contactsFilter.toLowerCase();
+// export const getVisibleContacts = (state) => {
+//   const allContacts = getContacts(state);
+//   const contactsFilter = getFiltredValue(state);
+//   const normalizeFilter = contactsFilter.toLowerCase();
 
-  return allContacts.filter((el) =>
-    el.name.toLowerCase().includes(normalizeFilter)
-  );
-};
-// export const getVisibleContacts = createSelector(
-//   [getContacts, getFiltredValue],
-//   (items, filter) => items.filter(items.name.toLowerCase().includes(filter))
-// );
+//   return allContacts.filter((el) =>
+//     el.name.toLowerCase().includes(normalizeFilter)
+//   );
+// };
+export const getVisibleContacts = createSelector(
+  [getContacts, getFiltredValue],
+  (items, filter) =>
+    items.filter((items) => items.name.toLowerCase().includes(filter))
+);
