5d4b5ca06dfb75d844b50f919a66fed47d5df62c
Julia_Vladomira <juliavladomira@gmail.com>
2023-02-09 13:59:20 +0100

    login+deploy

--- src/helpers/handler-passwordError.js
+++ src/helpers/handler-passwordError.js
@@ -1,7 +1,7 @@
 import { toast } from "react-toastify";
 
 export const isPasswordError = (value, setError, error) => {
-  if (value.length > 7) setError(false);
+  if (value.length >= 7) setError(false);
   else if (value.length < 7) {
     setError(true);
     error && toast.error("Please type minimum 7 characters");
